dc7842b0b59adb97a0f46e2074dd7810caaf5fd6
Rafael Kohler <rafael.kohler@gmail.com>
2019-05-30 20:40:43 -0300

    Projeto de cadastro de veículos

---
+++ veiculos/JavaSource/br/edu/dsj/scv/entidade/Marca.java
@@ -0,0 +1,44 @@
+package br.edu.dsj.scv.entidade;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * Representa a {@link Marca} de um Automóvel.
+ * 
+ * @author 4694
+ *
+ */
+public class Marca {
+
+	@NotBlank
+	private String nome;
+
+	@NotBlank
+	private String descricao;
+
+	public Marca() {
+
+	}
+
+	public Marca(String nome, String descricao) {
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
